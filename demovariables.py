cef089f0492725b01e9becc88906f7553ec6d1b7
Git User <some@example.com>
2023-10-02 11:25:06 +0100

    Add demo with variables

---
+++ demovariables.py
@@ -0,0 +1,11 @@
+username='Bob'
+age=32
+print(username+'is'+str(age)+'years old')
+username=input('Please enter your name')
+age = input('Please enter your age')
+print(username,'is',age,'years old')
+
+length=int(input("Please enter the length of the rectangle: "))
+width=int(input("Please enter the width of the rectangle"))
+print("The area is",length*width)
+print("The perimeter is",length+width<<1)
