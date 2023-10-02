704bacfc3321fee6e35eb884d0f7c76c2724dbdb
andreistefanenache <146005381+andreistefanenache@users.noreply.github.com>
2023-10-02 15:04:21 +0000

    Add Investment.py

---
+++ Investment.py
@@ -0,0 +1,9 @@
+"""
+Task 3 - Investment 
+1.	Add a new file: Investment.py and make it the startup file.  
+2.	Calculates how many years it will take an initial investment of £100 to grow to a target value of £1000 if the interest rate is 10%.
+Tip: Do not start writing code until you've a plan of action!
+3.	Save and run.   
+ """
+from math import log
+print(log(1000/100,1.1),"years")
